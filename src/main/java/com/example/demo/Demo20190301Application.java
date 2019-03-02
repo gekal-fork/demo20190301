f60d7bd4631e6bd1a27fa99bea2e3bbeed4941c4
yosuke0517 <x.clover.x007@gmail.com>
2019-03-02 15:40:00 +0900

    init commit

---
+++ src/main/java/com/example/demo/Demo20190301Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo20190301Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo20190301Application.class, args);
+	}
+
+}
